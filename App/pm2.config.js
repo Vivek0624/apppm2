99315585680b3f5da9879851c605ee29debc0367
Vivek Gundu <62993315+Vivek0624@users.noreply.github.com>
2023-10-11 12:36:51 +0530

    Create pm2.config.js

---
+++ App/pm2.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "dev",
+      script: "npm run dev",
+    },
+    {
+      name: "prod",
+      script: "npm run prod",
+    },
+    {
+      name: "prod-deploy",
+      script: "npm run deploy",
+    },
+  ],
+};
